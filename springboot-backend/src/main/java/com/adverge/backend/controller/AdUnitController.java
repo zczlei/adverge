46c092c33e9524de680f3ca8f4c515e758d51840
zczlei <zczlei@gmail.com>
2025-04-19 00:58:37 +0800

    添加Maven Wrapper并更新控制器实现

--- springboot-backend/src/main/java/com/adverge/backend/controller/AdUnitController.java
+++ springboot-backend/src/main/java/com/adverge/backend/controller/AdUnitController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/ad-units")
+@RequestMapping("/api/ad-units")
 @RequiredArgsConstructor
 public class AdUnitController {
 
--- springboot-backend/src/main/java/com/adverge/backend/controller/AppController.java
+++ springboot-backend/src/main/java/com/adverge/backend/controller/AppController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/apps")
+@RequestMapping("/api/apps")
 @RequiredArgsConstructor
 public class AppController {
 
