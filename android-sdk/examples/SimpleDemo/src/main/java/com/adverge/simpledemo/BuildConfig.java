576e675b76b39cf907c13a6893f79bbca4913692
Your Name <you@example.com>
2025-04-21 17:05:32 +0800

    添加SimpleDemo示例项目

---
+++ android-sdk/examples/SimpleDemo/src/main/java/com/adverge/simpledemo/BuildConfig.java
@@ -0,0 +1,17 @@
+package com.adverge.simpledemo;
+
+/**
+ * 广告单元ID和配置常量
+ */
+public class BuildConfig {
+    // 应用ID
+    public static final String APP_ID = "demo_app_id";
+    
+    // 广告单元ID
+    public static final String BANNER_AD_UNIT_ID = "demo_banner_ad_unit_id";
+    public static final String INTERSTITIAL_AD_UNIT_ID = "demo_interstitial_ad_unit_id";
+    public static final String REWARDED_AD_UNIT_ID = "demo_rewarded_ad_unit_id";
+    
+    // DEBUG模式
+    public static final boolean DEBUG = true;
+} 
